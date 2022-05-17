396b0ca06f785f2feafec65a556e66a8124ef194
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-05-16 19:55:53 -0700

    hello world working server

---
+++ api/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// Authentication libraries
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+// Initiate our application
+const app = express();
+
+// Enable all origins to connect to our app
+app.use(cors());
+
+// Global middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Database Set up
+const mongoDB = process.env.MONGO_DB;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+
+// Set up local strategy for our authentication (passport.authentication() uses these settings)
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      // Error occurred in our search
+      if (err) return done(err);
+
+      // Validates username
+      if (!user) {
+        return done(null, false);
+      }
+
+      // Validates password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) return done(err);
+
+        if (res) {
+          // Password authenticated
+          return done(null, user);
+        } else {
+          // passwords do not match
+          return done(null, false);
+        }
+      });
+    });
+  })
+);
+
+app.get("/", (req, res, next) => {
+  res.json({ msg: "hello world" });
+});
+
+module.exports = app;
