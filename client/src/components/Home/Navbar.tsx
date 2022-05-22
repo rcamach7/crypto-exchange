109fe8847adaa6153ec40926e80d3d39d26a4a38
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-05-22 14:39:41 -0700

    remove searchbar

--- client/src/components/Home/Navbar.tsx
+++ client/src/components/Home/Navbar.tsx
@@ -11,52 +11,6 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import { useUserContext } from "../../hooks/useUserContext";
 import logo from "../../data/crypto.png";
-import { styled, alpha } from "@mui/material/styles";
-import InputBase from "@mui/material/InputBase";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
 
 export const Navbar = () => {
   const { user, setToken } = useUserContext();
@@ -81,20 +35,10 @@ export const Navbar = () => {
     <AppBar position="static" className="Navbar">
       <Container maxWidth="xl" className="navbarContainer">
         <Toolbar disableGutters>
-          {/* Mobile Version */}
           <Avatar alt="logo" src={logo} sx={{ mr: 2 }} />
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
 
           {/* Profile Icon with sub-menu */}
-          <Box sx={{ flexGrow: 0, marginLeft: "auto", paddingLeft: "10px" }}>
+          <Box sx={{ flexGrow: 0, marginLeft: "auto" }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt={user?.username} src="./" className="userAvatar" />
