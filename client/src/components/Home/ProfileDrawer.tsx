77cc7e06ddb9122d87cdf1ad0543b135b2897704
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-05-24 13:18:55 -0700

    add boilerplate drawer and implement into navbar button

---
+++ client/src/components/Home/ProfileDrawer.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { Typography } from "@mui/material";
+
+type Anchor = "top" | "left" | "bottom" | "right";
+
+export const ProfileDrawer = () => {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  return (
+    <div className="profileButton">
+      <Typography onClick={toggleDrawer("right", true)}>Profile</Typography>
+      <Drawer
+        anchor="right"
+        open={state["right"]}
+        onClose={toggleDrawer("right", false)}
+      >
+        {list("right")}
+      </Drawer>
+    </div>
+  );
+};
--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "../components/Home/Navbar";
 import { useUserContext } from "../hooks/useUserContext";
 import { CryptoCard } from "../components/Home/CryptoCard";
+import { ProfileDrawer } from "../components/Home/ProfileDrawer";
 
 export const Home = () => {
   const { cryptos } = useUserContext();
@@ -13,6 +14,7 @@ export const Home = () => {
           return <CryptoCard key={crypto.ticker} crypto={crypto} />;
         })}
       </div>
+      <ProfileDrawer />
     </div>
   );
 };
