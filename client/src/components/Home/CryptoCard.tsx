b3f23d5ea9a1cf903edde9c904dfc4357df823f9
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-05-23 20:33:31 -0700

    customize MUI components to fit crypto data

--- client/src/components/Home/CryptoCard.tsx
+++ client/src/components/Home/CryptoCard.tsx
@@ -6,35 +6,97 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Chip from "@mui/material/Chip";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 
 interface Props {
   crypto: Crypto;
 }
 
 export const CryptoCard: React.FC<Props> = ({
-  crypto: { name, image, lastUpdated, marketHistory, price, ticker },
+  crypto: {
+    name,
+    image,
+    lastUpdated,
+    marketHistory: {
+      priceChangePercentage14d,
+      priceChangePercentage24h,
+      priceChangePercentage7d,
+    },
+    price,
+    ticker,
+  },
 }) => {
   return (
-    <Box sx={{ minWidth: 275 }} className="CryptoCard">
+    <Box sx={{ minWidth: 300 }} className="CryptoCard">
       <Card variant="outlined">
         <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
+          <Typography sx={{ fontSize: 12 }} color="text.secondary" gutterBottom>
+            price updated 10 minutes ago
           </Typography>
-          <Typography variant="h5" component="div">
-            hello
+
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{ display: "flex", alignItems: "center", gap: "5px" }}
+          >
+            <Avatar
+              sx={{ border: "solid black 1px" }}
+              aria-label="recipe"
+              src={image}
+            />
+            {name}
+            <Chip
+              sx={{ marginLeft: "auto" }}
+              label={price}
+              color={priceChangePercentage24h > 0 ? "success" : "error"}
+              icon={<AttachMoneyIcon fontSize="small" />}
+            />
           </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
+          <Typography
+            sx={{ marginTop: "5px" }}
+            fontSize="small"
+            // fontWeight="bold"
+          >
+            Price History
             <br />
-            {'"a benevolent smile"'}
+            <div className="priceHistoryChips">
+              <Chip
+                sx={{ paddingLeft: "2px" }}
+                color={priceChangePercentage24h > 0 ? "success" : "error"}
+                variant="outlined"
+                size="small"
+                label={`${priceChangePercentage24h}%`}
+                avatar={
+                  <Avatar sx={{ backgroundColor: "transparent" }}>24h</Avatar>
+                }
+              />
+              <Chip
+                sx={{ paddingLeft: "2px" }}
+                color={priceChangePercentage7d > 0 ? "success" : "error"}
+                variant="outlined"
+                size="small"
+                label={`${priceChangePercentage7d}%`}
+                avatar={
+                  <Avatar sx={{ backgroundColor: "transparent" }}>7d</Avatar>
+                }
+              />
+              <Chip
+                sx={{ paddingLeft: "2px" }}
+                color={priceChangePercentage14d > 0 ? "success" : "error"}
+                variant="outlined"
+                size="small"
+                label={`${priceChangePercentage14d}%`}
+                avatar={
+                  <Avatar sx={{ backgroundColor: "transparent" }}>14d</Avatar>
+                }
+              />
+            </div>
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small">Learn More</Button>
+          <Button size="small">Purchase {ticker}</Button>
         </CardActions>
       </Card>
     </Box>
