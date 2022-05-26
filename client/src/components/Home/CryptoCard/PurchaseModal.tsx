27ae17367ecede1fbc712d6ab8d3bc6ccc5f62d1
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-05-26 14:22:41 -0700

    finish visual of purchase modal

--- client/src/components/Home/CryptoCard/PurchaseModal.tsx
+++ client/src/components/Home/CryptoCard/PurchaseModal.tsx
@@ -6,10 +6,13 @@ import {
   Modal,
   TextField,
   Avatar,
+  Chip,
 } from "@mui/material/";
 import { Crypto } from "../../../data/models";
 import { capitalizeFirstLetter } from "../../../assets/helpers";
 import { formatPrice } from "../../../assets/helpers";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import { useUserContext } from "../../../hooks/useUserContext";
 
 const style = {
   position: "absolute" as "absolute",
@@ -21,6 +24,7 @@ const style = {
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+  paddingBottom: "5px",
 };
 
 interface Props {
@@ -28,9 +32,14 @@ interface Props {
 }
 
 export const PurchaseModal: React.FC<Props> = ({ crypto }) => {
+  const { user } = useUserContext();
   const [open, setOpen] = React.useState(false);
+  const [quantity, setQuantity] = React.useState<number>(0);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const handlePurchase = () => {};
 
   return (
     <div>
@@ -44,31 +53,66 @@ export const PurchaseModal: React.FC<Props> = ({ crypto }) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            sx={{ textAlign: "center" }}
+          >
             Purchase {capitalizeFirstLetter(crypto.name)}
           </Typography>
           <form className="PurchaseCryptoForm">
             <div className="priceOverview">
-              <Avatar
-                sx={{ border: "solid black 1px" }}
-                aria-label="crypto"
-                src={crypto.image}
-              />
-              <p>${formatPrice(crypto.price)}</p>
+              <div className="cryptoDetails">
+                <Avatar
+                  sx={{ border: "solid black 1px" }}
+                  aria-label="crypto"
+                  src={crypto.image}
+                />
+                <div className="buyPrice">
+                  <p className="buyPrice">Buy Price</p>
+                  <p>${formatPrice(crypto.price)}</p>
+                </div>
+              </div>
+              <div className="balanceDetails">
+                <Avatar
+                  sx={{ border: "solid black 1px", backgroundColor: "black" }}
+                  aria-label="balance"
+                >
+                  <AccountBalanceWalletIcon />
+                </Avatar>
+                <div className="balance">
+                  <p className="balance">Balance</p>
+                  <p>${formatPrice(user?.balance)}</p>
+                </div>
+              </div>
             </div>
-            <TextField
-              className="quantityInput"
-              id="outlined-number"
-              label="Enter Quantity"
-              type="number"
-              size="small"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              InputProps={{
-                inputProps: { min: 0, defaultValue: 0 },
-              }}
-            />
+
+            <div className="checkoutDetails">
+              <TextField
+                className="quantityInput"
+                id="outlined-number"
+                label="Enter Quantity"
+                type="number"
+                size="small"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                InputProps={{
+                  inputProps: { min: 0 },
+                }}
+                value={quantity}
+                onChange={(e) => setQuantity(Number.parseInt(e.target.value))}
+              />
+
+              <p className="totalCalculation">
+                Total: ${formatPrice(quantity * crypto.price)}
+              </p>
+            </div>
+
+            <Button className="purchaseBtn" variant="contained">
+              Confirm Purchase
+            </Button>
           </form>
         </Box>
       </Modal>
