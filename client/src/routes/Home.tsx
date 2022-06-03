7dbfcc4d62d5380b2e81471dbe2780bde4be3c8c
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-06-03 11:39:23 -0700

    add radio buttons for filtering

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,10 +1,35 @@
 import { useGlobalContext } from "../hooks/useGlobalContext";
 import { CryptoCard } from "../components/Home/CryptoCard/CryptoCard";
+import {
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+} from "@mui/material/";
 
 export const Home = () => {
   const { cryptos, user } = useGlobalContext();
   return (
     <div className="Home">
+      <div className="filterOptions">
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Filter By</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+            />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+          </RadioGroup>
+        </FormControl>
+      </div>
       <div className="cryptosContainer">
         {cryptos.map((crypto) => {
           return <CryptoCard key={crypto.ticker} crypto={crypto} user={user} />;
