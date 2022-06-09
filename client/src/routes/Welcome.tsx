e67ca4f285d8849547d9a34dbfefb4f69cd04a00
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-06-09 13:57:44 -0700

    add wrapper for features and implement basic boilerplate list of features

--- client/src/routes/Welcome.tsx
+++ client/src/routes/Welcome.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import GrainIcon from "@mui/icons-material/Grain";
 
-const IntroCard = styled.div`
+const IntroCardWrapper = styled.div`
   max-width: 90%;
   padding: clamp(20px, 3vw, 50px);
 
@@ -29,16 +30,45 @@ const IntroCard = styled.div`
   }
 `;
 
+const FeaturesWrapper = styled.ul`
+  padding: clamp(20px, 3vw, 50px);
+
+  list-style: none;
+
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  li {
+    display: flex;
+    align-items: center;
+    .icon {
+      border-right: solid 1px black;
+    }
+  }
+`;
+
 export const Welcome: React.FC = () => {
   return (
     <div className="Welcome">
-      <IntroCard>
+      <IntroCardWrapper>
         <p className="subIntroText">Start with $1,000,000</p>
         <h1>Practice crypto trading with virtual money!</h1>
         <Link className="actionButton" to="/home">
-          <p>Start Investing</p>
+          <p>Browse Cryptos</p>
         </Link>
-      </IntroCard>
+      </IntroCardWrapper>
+
+      <FeaturesWrapper>
+        <li>
+          <GrainIcon className="icon" /> Hello World
+        </li>
+        <li>
+          <GrainIcon className="icon" /> Hello World
+        </li>
+        <li>
+          <GrainIcon className="icon" /> Hello World
+        </li>
+      </FeaturesWrapper>
     </div>
   );
 };
