1eebee3c11dd12a0fa0e2be46e4d4ee43d6e2726
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-06-12 16:44:20 -0700

    max sizing for really large viewports

--- client/src/routes/Welcome.tsx
+++ client/src/routes/Welcome.tsx
@@ -17,18 +17,18 @@ const IntroCardWrapper = styled.div`
   justify-content: center;
   flex-direction: column;
   .subIntroText {
-    font-size: clamp(15px, 1.5vw, 25px);
+    font-size: clamp(15px, 1.5vw, 20px);
     margin-bottom: clamp(10px, 1vw, 30px);
   }
   h1 {
-    font-size: clamp(30px, 4vw, 60px);
+    font-size: clamp(30px, 4vw, 50px);
   }
   .actionButton {
-    width: clamp(150px, 15vw, 225px);
+    width: clamp(150px, 15vw, 190px);
     padding: 10px;
     margin-top: clamp(25px, 10vw, 80px);
 
-    font-size: clamp(15px, 1.5vw, 25px);
+    font-size: clamp(15px, 1.5vw, 20px);
     text-align: center;
 
     color: white;
