7f14dd67e99713c89f9e98bed862ed38abb69fea
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-07-04 21:39:52 -0700

    isolate toolbar components into their own folder and parent component to separate logic from homepage

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,26 +1,21 @@
 import { useGlobalContext } from "../context/GlobalCryptoContext";
 import { CryptoCard } from "../components/Home/CryptoCard";
-import { Crypto, SortFilterOptions } from "../data/global.models";
-import React, { useState, useEffect } from "react";
+import { Crypto } from "../data/global.models";
+import { useState, useEffect } from "react";
 import {
-  processFilterSortOptions,
   replaceUpdatedCrypto,
   determineThemeBackground,
 } from "../utilities/helpers";
-import { SortFilterBar } from "../components/Home/SortFilterBar";
 import { updateSingleCrypto } from "../data/api";
 import { useTheme } from "@mui/material/styles";
 import Pagination from "@mui/material/Pagination";
-import Autocomplete from "@mui/material/Autocomplete";
-import { TextField } from "@mui/material";
+import { ToolBar } from "../components/Home/ToolBar/ToolBar";
 
 export const Home = () => {
   const { cryptos, user, togglePageLoading, setUser, handleBannerMessage } =
     useGlobalContext();
   const [organizedCryptos, setOrganizedCryptos] = useState<Crypto[]>([]);
-  const [sortFilterOptions, setSortFilterOptions] = useState<SortFilterOptions>(
-    { sort: "popular", filter: "none" }
-  );
+
   const [page, setPage] = useState(1);
   let ranges = [
     [0, 9],
@@ -28,30 +23,13 @@ export const Home = () => {
     [18, 27],
     [27, 36],
   ];
-
   const theme = useTheme();
-
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    setPage(value);
-  };
-
-  const handleSearch = (
-    e: React.SyntheticEvent<Element, Event>,
-    value: string | null
-  ) => {
-    if (value) {
-      setOrganizedCryptos(
-        organizedCryptos.filter((curCrypto) => curCrypto.name.includes(value))
-      );
-    } else {
-      setOrganizedCryptos(cryptos);
-    }
-  };
 
   useEffect(() => {
     togglePageLoading();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   useEffect(() => {
     if (cryptos.length) {
       togglePageLoading();
@@ -59,19 +37,6 @@ export const Home = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cryptos]);
-
-  useEffect(() => {
-    setOrganizedCryptos(
-      processFilterSortOptions(
-        cryptos,
-        sortFilterOptions,
-        user ? user.portfolio : [],
-        user ? user.bookmarks : []
-      )
-    );
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sortFilterOptions, user?.bookmarks]);
 
   const handleUpdateSingleCrypto = async (name: string) => {
     togglePageLoading();
@@ -94,22 +59,13 @@ export const Home = () => {
         backgroundColor: `${determineThemeBackground(theme.palette.mode)}`,
       }}
     >
-      <div className="toolBar">
-        <SortFilterBar
-          setSortFilterOptions={setSortFilterOptions}
-          loggedIn={user ? true : false}
-          theme={theme.palette.mode}
-        />
-        <Autocomplete
-          id="free-solo-demo"
-          freeSolo
-          options={organizedCryptos.map((crypto) => crypto.name)}
-          renderInput={(params) => <TextField {...params} label="Search..." />}
-          sx={{ minWidth: "200px" }}
-          size="small"
-          onChange={(e, value) => handleSearch(e, value)}
-        />
-      </div>
+      <ToolBar
+        user={user}
+        cryptos={cryptos}
+        organizedCryptos={organizedCryptos}
+        setOrganizedCryptos={setOrganizedCryptos}
+      />
+
       <div className="cryptosContainer">
         {organizedCryptos
           .slice(ranges[page - 1][0], ranges[page - 1][1])
@@ -130,7 +86,7 @@ export const Home = () => {
       <Pagination
         count={Math.ceil(organizedCryptos.length / 9)}
         page={page}
-        onChange={handleChange}
+        onChange={(e, value) => setPage(value)}
       />
     </div>
   );
