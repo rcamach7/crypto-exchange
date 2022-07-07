94c91298d3f4579e14379db8727d61f7159da5e5
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-07-07 12:23:44 -0700

    create news article schema

---
+++ api/models/NewsArticle.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+const NewsArticle = new Schema({
+  title: { type: String, required: true },
+  link: { type: String, required: true },
+  creator: { type: String, required: true },
+  description: { type: String, required: true },
+  content: { type: String, required: true },
+  pubDate: { type: Date, required: true },
+  image_url: { type: String, required: true },
+});
+
+module.exports = model("NewsArticle", NewsArticle);
