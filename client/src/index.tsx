9f45fd4cd480991347742f9758c56295296c4467
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-09-01 14:02:12 -0700

    chore: remove no longer needed scss dependency

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import "./styles/index.scss";
+import "./styles/index.css";
 import ReactDOM from "react-dom/client";
 import { RouteSwitch } from "./RouteSwitch";
 import { GlobalCryptoProvider } from "./context/GlobalCryptoContext";
