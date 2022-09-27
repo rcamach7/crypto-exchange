eb42bd2dd44ff02412ab5a5950648e0f1abdeef5
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-09-27 12:16:43 -0700

    style: Clean up NewsCard component styling

--- client/src/components/Home/NewsSectionTitle.tsx
+++ client/src/components/Home/NewsSectionTitle.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import OnlinePredictionIcon from "@mui/icons-material/OnlinePrediction";
-import { Theme } from "@mui/material";
+import { Theme, Typography } from "@mui/material";
 
 interface Props {
   count: number;
@@ -9,11 +9,12 @@ interface Props {
 
 export const NewsSectionTitle: React.FC<Props> = ({ count, theme }) => {
   return (
-    <span style={{ textAlign: "center", padding: "25px 0 15px 0" }}>
+    <span style={{ textAlign: "center", padding: "41px 0 15px 0" }}>
       {count ? (
         // Title will be non-existent if news articles have yet to be loaded.
         <span
           style={{
+            width: 300,
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
@@ -25,7 +26,9 @@ export const NewsSectionTitle: React.FC<Props> = ({ count, theme }) => {
             color="success"
             className="animate__animated animate__fadeIn animate__infinite animate__slower"
           />
-          Trending News
+          <Typography sx={{ textDecoration: "underline" }}>
+            Trending News
+          </Typography>
           <OnlinePredictionIcon
             color="success"
             className="animate__animated animate__fadeIn animate__infinite animate__slower"
